625bd48890928a9e22057b2e0590b77d28d97905
egenvall <egenvall.kim@gmail.com>
2016-11-03 12:49:20 +0100

    Skeleton for Toolbox controller

---
+++ app/toolbox/toolbox.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('myApp.toolBox', ['ngRoute'])
+
+//Don't really need routing do we since it is not separate pages
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/toolBox', {
+    templateUrl: 'toolbox/toolbox.html',
+    controller: 'ToolboxController'
+  });
+}])
+
+.controller('ToolboxController', [function() {
+
+}]);
