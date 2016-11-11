68aa151e4c4ad4bdfb905cd7d6d634baf341e0b1
soulsolomon <mislusolo@gmail.com>
2016-11-11 13:19:32 +0100

    Update operation.js
    operationimpl

--- app/operation/operation.js
+++ app/operation/operation.js
@@ -1,5 +1,17 @@
 'use strict';
 angular.module('myApp.operation', ['ngRoute'])
-.controller('OperationController', [function() {
+.controller('OperationController', [function OperationController($scope) {
 //Implement Methods
+   $scope.name = "Operation";
+    $scope.position = null;
+    $scope.init = function(position){
+        $scope.position = position;
+    }
+    $scope.setName = function(name){
+        $scope.name = name;
+    }
+    $scope.updatePosition = function(position){
+        $scope.position = position;
+    }
+}]);
 }]);
