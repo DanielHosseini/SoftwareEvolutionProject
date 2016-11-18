6cf7deaaf0f2e99c37bf1663dee9075bd5bf51cb
Isabelle Frölich <isabellefrolich@hotmail.com>
2016-11-18 13:17:36 +0100

    Package controller added

--- app/package/package.js
+++ app/package/package.js
@@ -1,7 +1,26 @@
 'use strict';
-angular.module('myApp')
-.controller('PackageController', function() {
-//Implement Methods
+var myApp = angular.module('myApp')
+.controller('PackageController', function PackageController($scope) {
+
+	$scope.name = "Package";
+	$scope.classes = [];
+    $scope.position = null;
+    $scope.init = function(position){
+        $scope.position = position;
+    }
+    $scope.setName = function(name){
+        $scope.name = name;
+    }
+    $scope.addClass = function(class){
+        $scope.classes.push(class);
+    }
+    $scope.getClasses = function(){
+        return $scope.classes;
+    }
+    $scope.updatePosition = function(position){
+        $scope.position = position;
+    }
+    
 }).directive('packageDirective', function(){
     return {
         templateUrl: 'package/package.html'
