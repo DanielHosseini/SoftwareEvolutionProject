52f924bb312976694356ff3533498929300808df
Olliver Mattsson <mattsson_93@hotmail.com>
2016-11-23 10:55:48 +0100

    Removed unnecessary code from jsplumbwrapper

--- app/wrapper/jsPlumbWrapperController.js
+++ app/wrapper/jsPlumbWrapperController.js
@@ -179,39 +179,9 @@ myApp.directive('plumbMenuItem', function() {
 			// jsPlumb uses the containment from the underlying library, in our case that is jQuery.
 			jsPlumb.draggable(element, {
                 stop: function(event, ui){
-                    
+                    console.log("dropped")
                 },
 				containment: false
-			});
-		}
-	};
-});
-
-myApp.directive('droppable', function($compile) {
-	return {
-		restrict: 'A',
-		link: function(scope, element, attrs){
-			//console.log("Make this element droppable");
-
-			element.droppable({
-				drop:function(event,ui) {
-					// angular uses angular.element to get jQuery element, subsequently data() of jQuery is used to get
-					// the data-identifier attribute
-					var dragIndex = angular.element(ui.draggable).data('identifier'),
-					dragEl = angular.element(ui.draggable),
-					dropEl = angular.element(this);
-
-					// if dragged item has class menu-item and dropped div has class drop-container, add module
-					if (dragEl.hasClass('menu-item') && dropEl.hasClass('drop-container')) {
-						//console.log("Drag event on " + dragIndex);
-						var x = event.pageX - scope.module_css.width / 2;
-						var y = event.pageY - scope.module_css.height / 2;
-
-						scope.addModuleToSchema(dragIndex, x, y);
-					}
-
-					scope.$apply();
-				}
 			});
 		}
 	};
