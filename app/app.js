45c6b44143deb0b4267b57a048ecc27acfbc63f0
Olliver Mattsson <mattsson_93@hotmail.com>
2016-11-29 13:09:29 +0100

    Now asks before clearing diagram

--- app/app.js
+++ app/app.js
@@ -35,6 +35,22 @@ myApp.controller('AppController', ['$scope', 'diagramService', function($scope, 
     }
 
     $scope.clear  = function(){
+        console.log("clearing")
         diagramService.clearAll();
     }
 }]);
+
+myApp.directive('ngConfirmClick', [function(){
+    return{
+        link: function (scope, element, attr) {
+            var msg = attr.ngConfirmClick || "Are you sure?";
+            var clickAction = attr.confirmedClick;
+            element.bind('click',function (event) {
+                if ( window.confirm(msg) ) {
+                    console.log("confirmed")
+                    scope.$eval(clickAction);
+                }
+            });
+        }
+    }
+}])
