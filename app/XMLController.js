cc4fac18c94bcfe219b8d2524aec688516aabcef
DanielH <daniel.hosseini1@gmail.com>
2016-11-29 16:40:28 +0100

    added file xmlcontroller and linked it to index

---
+++ app/XMLController.js
@@ -0,0 +1,11 @@
+'use strict';
+var myApp = angular.module('myApp');
+myApp.controller('XMLController', function ($scope) {
+
+ $scope.alert = function(position){
+		alert("Hello")
+
+	   }
+
+
+});
