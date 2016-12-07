782b7079325b4b8e186a0583c0f00e20a310896e
DanielH <daniel.hosseini1@gmail.com>
2016-12-07 22:48:47 +0100

    removed some unused code and added proper html structure

--- app/app.js
+++ app/app.js
@@ -57,7 +57,7 @@ myApp.directive('ngConfirmClick', [function(){
         link: function (scope, element, attr) {
             var msg = attr.ngConfirmClick || "Are you sure?";
             var clickAction = attr.confirmedClick;
-            element.bind('click',function (event) {
+            element.bind('click',function () {
                 if ( window.confirm(msg) ) {
                     scope.$eval(clickAction);
                 }
