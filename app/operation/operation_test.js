c4d71f5e69479679992f27e3a6eaa7b40379b5bf
Olliver Mattsson <mattsson_93@hotmail.com>
2016-12-09 10:15:40 +0100

    Fixed failing tests

--- app/operation/operation_test.js
+++ app/operation/operation_test.js
@@ -12,19 +12,9 @@ describe('OperationController', function() {
         it('changes the name of the Operation() to doSomething()', function(){
             var $scope = {};
             var controller = $controller('OperationController', {$scope: $scope});
-            $scope.name = 'Operation()';
-            $scope.setName('doSomething()');
-            expect($scope.name).toEqual('doSomething()');
+            $scope.operationTemplate.name = 'Operation()';
+            $scope.operationTemplate.setName('doSomething()');
+            expect($scope.operationTemplate.name).toEqual('doSomething()');
         });
     });
-
-    describe('$scope.position', function(){
-        it('updates the position to be (150,50)', function(){
-            var $scope = {};
-            var controller = $controller('OperationController', {$scope: $scope});
-            $scope.updatePosition({x: 150, y: 50});
-            expect($scope.position.x).toEqual(150);
-            expect($scope.position.y).toEqual(50);
-        })
-    })
 });
